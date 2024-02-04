37d8e0272653521f8e37910e47416760cc9501a2
mikerly131 <mrr5068@gmail.com>
2024-02-04 12:40:39 -0500

    setting up async sessions for sqlalchemy and sqlite db

--- data_models/db_session.py
+++ data_models/db_session.py
@@ -3,15 +3,21 @@ from typing import Callable, Optional
 
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
+# it really hates these imports and idk why
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine, AsyncSession
 from data_models.modelbase import SQLAlchemyBase
 from sqlalchemy.orm import Session
 
+
+# For sync sqlite/sqlalchemy
 __factory: Optional[Callable[[], Session]] = None
+# For async sqlite/sqlalchemy
+__async_engine: Optional[AsyncEngine] = None
 
 
 # takes in a db, initializes it
 def global_init(db_file: str):
-    global __factory
+    global __factory, __async_engine
 
     if __factory:
         return
@@ -22,13 +28,15 @@ def global_init(db_file: str):
     folder = Path(db_file).parent
     folder.mkdir(parents=True, exist_ok=True)
 
-    conn_str = 'sqlite:///' + db_file.strip()
+    conn_str = 'sqlite+pysqlite:///' + db_file.strip()
+    async_conn_str = 'sqlite+aiosqlite:///' + db_file.strip()
     print('Connecting to DB with {}'.format(conn_str))
 
     # Adding check_same_thread = False after the recording. This can be an issue about
     # creating / owner thread when cleaning up sessions, etc. This is a sqlite restriction
     # that we probably don't care about in this example.
     engine = sa.create_engine(conn_str, echo=False, connect_args={'check_same_thread': False})
+    __async_engine = create_async_engine(async_conn_str, echo=False, connect_args={'check_same_thread': False})
     __factory = orm.sessionmaker(bind=engine)
 
     # noinspection PyUnresolvedReferences
@@ -46,5 +54,17 @@ def create_session() -> Session:
 
     session: Session = __factory()
     session.expire_on_commit = False
+
+    return session
+
+
+def create_async_session() -> AsyncSession:
+    global __async_engine
+
+    if not __async_engine:
+        raise Exception('You must call global_init() async before using this method.')
+
+    session: AsyncSession = AsyncSession(__async_engine)
+    session.sync_session.expire_on_commit = False
 
     return session
